f8ebe347609a5ce7637ab6b96675f1b7e343a364
gilszon <gilszon@gmail.com>
2022-07-15 17:34:39 -0300

    Modify icon when send

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
-import { toast } from "react-toastify";
+//import { toast } from "react-toastify";
 import { BASE_URL } from "../../utils/request";
 import icon from "../../assets/img/notification-icon.svg";
+import iconsend from "../../assets/img/icon-send.png";
 import './styles.css';
+import { useState } from "react";
 
 type Props = {
     saleId: number;
@@ -10,16 +12,28 @@ type Props = {
 
 function NotificationButton({saleId} : Props) {
 
+    const [theicon, setTheIcon]  = useState(icon);
+    
+    const [classEffect, setclassEffect]  = useState("dsmeta-red-btn");
+
+
+
     function handleClick(id : number){
+
+        setTheIcon(iconsend)
+        setclassEffect("dsmeta-green-btn")
         axios(`${BASE_URL}/sales/${id}/notification`)
-            .then( response => {
-                toast.info("Sent SMS with success");
-            });
+            // .then( response => {
+            //     // toast.info("Sent SMS with success");
+                
+            // });
+
+            setTimeout(() => {  setTheIcon(icon); setclassEffect("dsmeta-red-btn")}, 5000);
     }
 
     return (
-        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
-            <img src={icon} alt="Notification" />
+        <div className={classEffect} onClick={() => handleClick(saleId)}>
+            <img src={theicon} alt="Notification" />
         </div>
     )
 }
