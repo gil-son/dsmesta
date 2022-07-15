dfd8671809d662ca913337ffffb68915f2b4753a
gilszon <gilszon@gmail.com>
2022-07-15 19:23:45 -0300

    Header - infos

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,10 +7,10 @@ function Header() {
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
-                <p>
+                <p>&nbsp;&nbsp;
                     Developed by
                     <a href="https://gil-son.github.io" target="_blank"> <u>Gilson Vieira de Souza</u></a>&nbsp;
-                     in partnership with <a href="https://devsuperior.com.br/" target="_blank"> <u>DevSuperior</u></a>
+                     and <a href="https://devsuperior.com.br/" target="_blank"> <u>DevSuperior</u></a>
                 </p>
             </div>
         </header>
