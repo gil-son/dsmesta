d189fa47746413701c501c4ddeb23a9a8c6edcf7
gilszon <gilszon@gmail.com>
2022-07-15 11:15:15 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,23 @@
+import axios from "axios";
+import { BASE_URL } from "../../utils/request";
 import icon from "./../../assets/img/notification-icon.svg";
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function NotificationButton({saleId} : Props) {
+
+    function handleClick(id : number){
+        axios(`${BASE_URL}/sales/${id}/notification`)
+            .then( response => {
+                console.log("Success!!")
+            });
+    }
+
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
