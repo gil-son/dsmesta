1bc475df01edad20f967a1fedbc543fd8a3f23e5
gilszon <gilszon@gmail.com>
2022-07-16 21:15:28 -0300

    Organized Toastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
-//import { ToastContainer } from 'react-toastify';
+import React from 'react'
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
+
+window.React = React // To render toast after click in the button
 
 // ToastContainer can be called in any part of application - see in NotificationButton
 
 function App() {
   return (
     <>
-     {/* <ToastContainer /> */}
+     <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-//import { toast } from "react-toastify";
+import { toast } from "react-toastify";
 import { BASE_URL } from "../../utils/request";
 import icon from "../../assets/img/notification-icon.svg";
 import iconsend from "../../assets/img/icon-send.png";
@@ -23,13 +23,13 @@ function NotificationButton({saleId} : Props) {
         setTheIcon(iconsend)
         setclassEffect("dsmeta-green-btn")
         axios(`${BASE_URL}/sales/${id}/notification`)
-            // .then( response => {
-            //     // toast.info("Sent SMS with success");
-                
-            // });
+            .then( response => {
+                // toast.info("Sent SMS with success");
+                   toast.success("Sent SMS with success");
+            });
 
-            // toast.success("Sent SMS with success");
-            setTimeout(() => { setTheIcon(icon); setclassEffect("dsmeta-red-btn")}, 5000);
+            
+            setTimeout(() => { setTheIcon(icon); setclassEffect("dsmeta-red-btn")}, 6500);
     }
 
     return (
